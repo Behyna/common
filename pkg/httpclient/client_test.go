8a638f54c072903eb875ed13aac74ed8a4186c88
behyna.ansarirad <ansarirad.behyna@gmail.com>
2025-09-05 01:45:25 +0330

    fix: import path httpclient

--- pkg/httpclient/client_test.go
+++ pkg/httpclient/client_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Behyna/sms-services/smsgateway/pkg/httpclient"
+	"github.com/Behyna/common/pkg/httpclient"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
